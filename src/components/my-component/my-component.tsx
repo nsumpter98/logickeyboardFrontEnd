77cd7bff8721c11110b51e1b7846b18b7049dcc1
nsump <nsump@DESKTOP-RV8UIU1>
2022-02-07 11:07:51 -0500

    got api connection working, but started receiving the error "vNode passed as children has unexpected type.                                                              Make sure it's using the correct h() function.                                                              Empty objects can also be the cause, look for JSX comments that became objects."

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Host } from '@stencil/core';
+
 import '@ionic/core';
 @Component({
   tag: 'my-component',
@@ -8,45 +9,76 @@ import '@ionic/core';
 export class MyComponent {
 
 
-  render() {
+
+
+  async postButtonId() {
+    console.log('UPDATE WIRE PRICE');
+
+    let response = await fetch('https://localhost:7031/api/LogiCharsAPI');
+
+    let payload = await response.json();
+
+    return payload;
+
+    // do something
+  }
+
+
+ async render() {
+
+  let payload2 = await this.postButtonId();
+  console.log("API connector");
+  console.log(payload2);
+
     let payload = [
       {
-        'id': 1,
-        'explanation': 'implies; if ... then',
-        'unicode': 'U+2192',
-        'htmlValue': '&#8594;',
-        'htmlEntity': '&rarr;',
-        'laTeXSymbol': '\\to or',
-        'symbolChar': '→',
+        "id": 1,
+        "explanation": "implies; if ... then",
+        "unicode": "U+2192",
+        "htmlValue": "&#8594;",
+        "htmlEntity": "&rarr;",
+        "laTeXSymbol": "\\to \\to or",
+        "symbolChar": "→"
       },
       {
-        'id': 2,
-        'explanation': 'if and only if; iff; means the same as',
-        'unicode': 'U+27F7',
-        'htmlValue': '&#10231;',
-        'htmlEntity': '&#10231;',
-        'laTeXSymbol': '\\leftrightarrow',
-        'symbolChar': '⟷',
+        "id": 2,
+        "explanation": "if and only if; iff; means the same as",
+        "unicode": "U+27F7",
+        "htmlValue": "&#10231;",
+        "htmlEntity": "&#10231;",
+        "laTeXSymbol": "\\leftrightarrow",
+        "symbolChar": "⟷"
       },
       {
-        'id': 3,
-        'explanation': 'not',
-        'unicode': 'U+00AC',
-        'htmlValue': '&#172;',
-        'htmlEntity': '&not;',
-        'laTeXSymbol': '\\lnot or \\neg',
-        'symbolChar': '¬',
+        "id": 3,
+        "explanation": "not",
+        "unicode": "U+00AC",
+        "htmlValue": "&#172;",
+        "htmlEntity": "&not;",
+        "laTeXSymbol": "\\neg \\lnot or \\neg",
+        "symbolChar": "¬"
       },
       {
-        'id': 4,
-        'explanation': 'and',
-        'unicode': 'U+2227',
-        'htmlValue': '&#8743;',
-        'htmlEntity': '&and;',
-        'laTeXSymbol': '\\wedge or \\land',
-        'symbolChar': '∧',
+        "id": 5,
+        "explanation": "and",
+        "unicode": "U+2227",
+        "htmlValue": "&#8743;",
+        "htmlEntity": "&and;",
+        "laTeXSymbol": "\\wedge",
+        "symbolChar": "∧"
       },
+      {
+        "id": 6,
+        "explanation": "for all; for any; for each",
+        "unicode": "U+2200",
+        "htmlValue": "&#8704;",
+        "htmlEntity": "&forall;",
+        "laTeXSymbol": "\\forall",
+        "symbolChar": "∀"
+      }
     ];
+    console.log("Static payload")
+    console.log(payload);
 
     return(
       <Host>
